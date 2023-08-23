659ab6555099f4207d96ac1a6fed25876b4a8153
AA-FARUQ <aretheywhome@gmail.com>
2023-08-23 16:17:53 -0700

    work now!

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,21 +3,29 @@
 /**
  * main - Entry point of the program
  *
- * remember to follow all the necessary instructions
+ * This function is the starting point of the program's execution.
+ * It prints a simple message to the console and returns 0.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+int n;
+int a[5];
+int *p;
 
-	a[2] = 1024;
-	p = &n;
-	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+a[2] = 1024;
+p = &n;
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
